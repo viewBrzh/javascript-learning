a65e046f81b29e44fd39cbb63c74b6446fa84a8a
viewBrzh <watayut.pa@vulcan.bluebik.com>
2026-08-23 21:12:41 +0700

    closure

---
+++ closure.js
@@ -0,0 +1,14 @@
+function counter() {
+  let count = 0;
+
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const increment = counter();
+
+console.log(increment()); // 1
+console.log(increment()); // 2
+console.log(increment()); // 3
